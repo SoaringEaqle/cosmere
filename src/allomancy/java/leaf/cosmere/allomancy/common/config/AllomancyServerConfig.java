f3c3dff241df5711cf3a100c4cf6d72954d80374
Gerbagel <leongroth98@gmail.com>
2025-01-06 13:42:38 +0100

    [Allomancy] Added config option for iron/steel range, closes #135

--- src/allomancy/java/leaf/cosmere/allomancy/common/config/AllomancyServerConfig.java
+++ src/allomancy/java/leaf/cosmere/allomancy/common/config/AllomancyServerConfig.java
@@ -14,6 +14,7 @@ public class AllomancyServerConfig implements ICosmereConfig
 	private final ForgeConfigSpec configSpec;
 
 	public final ForgeConfigSpec.IntValue GOD_METAL_EAT_STRENGTH_MINIMUM;
+	public final ForgeConfigSpec.DoubleValue IRON_STEEL_RANGE;
 
 	// Boost amount for Duralumin and Nicrosil
 	//public final ForgeConfigSpec.DoubleValue boostAmount;
@@ -27,6 +28,9 @@ public class AllomancyServerConfig implements ICosmereConfig
 		GOD_METAL_EAT_STRENGTH_MINIMUM = builder.comment("When the user eats a lerasium nugget, what should their strength in allomancy be set to").defineInRange("godMetalMinimumStrength", 16, 1, 32);
 		//can't modify registry objects like that
 		//boostAmount = builder.comment("Boost amount for Duralumin and Nicrosil").defineInRange("boostAmount", 0.334D, 0D, 1D);
+
+		// max 10 might seem low, but the scanning thread chugs so hard at 10 that it's unusable and I doubt anyone will try it except for fun >> Gerbagel
+		IRON_STEEL_RANGE = builder.comment("Multiplier for iron and steel range. At base strength (9) the range is 9 while burning and 18 while flaring").defineInRange("ironSteelRange", 1.0D, 0D, 10.0D);
 
 		builder.pop();
 		configSpec = builder.build();
