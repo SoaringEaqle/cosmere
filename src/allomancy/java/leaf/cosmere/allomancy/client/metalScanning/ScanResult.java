0df71a3c960264b11649657236643d77fed1c419
Gerbagel <leongroth98@gmail.com>
2025-01-20 00:35:52 +0100

    Removed unused imports introduced via experimenting

--- src/allomancy/java/leaf/cosmere/allomancy/client/metalScanning/ScanResult.java
+++ src/allomancy/java/leaf/cosmere/allomancy/client/metalScanning/ScanResult.java
@@ -4,10 +4,7 @@
 
 package leaf.cosmere.allomancy.client.metalScanning;
 
-import leaf.cosmere.api.CosmereAPI;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
