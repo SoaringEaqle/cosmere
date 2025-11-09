867eb51a5e2afab059b6ca38afc10b0bc73304a4
Corey Kochman <coreykochman@gmail.com>
2025-11-09 18:40:22 -0500

    cleanup

--- src/datagen/main/java/leaf/cosmere/RecipeGen.java
+++ src/datagen/main/java/leaf/cosmere/RecipeGen.java
@@ -120,9 +120,6 @@ public class RecipeGen extends BaseRecipeProvider implements IConditionBuilder
 				decompressRecipe(consumer, ItemsRegistry.METAL_NUGGETS.get(metalType).get(), CosmereTags.Items.METAL_INGOT_TAGS.get(metalType), metalType.getName() + "_item_deconstruct");
 			}
 			else {
-				compressRecipe(ItemsRegistry.METAL_INGOTS.get(metalType).get(), ItemsRegistry.GOD_METAL_NUGGETS.get(metalType).get()).save(consumer);
-				decompressRecipe(consumer, ItemsRegistry.GOD_METAL_NUGGETS.get(metalType).get(), CosmereTags.Items.METAL_INGOT_TAGS.get(metalType), metalType.getName() + "_item_deconstruct");
-
 				godMetalNuggetRecipe(consumer, ItemsRegistry.METAL_NUGGETS.get(metalType).get());
 			}
 
